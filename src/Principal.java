758055ed47ea452f1908d314903f271019bec5d1
zPopeyex <stven-457@hotmail.com>
2022-08-31 22:43:51 -0500

    x

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,8 @@
 public class Principal {
 
-    public static void main(String[] args) {
-
-        Ventana v1 = new Ventana();
+public static void main(String[] args) {
+     Ventana v1 = new Ventana();
 
         v1.setVisible(true);
-
-    }
+}
 }
