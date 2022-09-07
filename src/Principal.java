8652764c9f3a4874d9c590fbe245f16fd333e9d7
Steven Leonardo Garzon Ospina <88815263+zPopeyex@users.noreply.github.com>
2022-09-07 15:43:51 -0500

    Calculator final update

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,7 @@
 public class Principal {
-
-public static void main(String[] args) {
-     Ventana v1 = new Ventana();
+    public static void main(String[] args) {
+        Ventana v1 = new Ventana();
 
         v1.setVisible(true);
-}
+    }
 }
--- src/Ventana.java
+++ src/Ventana.java
@@ -2,170 +2,165 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.*;
 
 public class Ventana extends JFrame implements ActionListener {
-	// Constructor
-	public Ventana() {
-		setTitle("Calculadora - Steven Garzón");
-		setSize(400, 500); // Tamaño ventana
+    private JLabel resulta;
 
-		initComponents();
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    // Constructor
+    public Ventana() {
+        setTitle("Calculadora - Steven Garzón");
+        setSize(400, 500); // Tamaño ventana
 
-	private void initComponents() {
+        initComponents();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-		// Panel contenedor
-		JPanel contenedor = new JPanel();
-		contenedor.setLayout(new GridLayout(2, 1));
+    private void initComponents() {
+        // Panel contenedor
+        JPanel contenedor = new JPanel();
+        contenedor.setLayout(new GridLayout(2, 1));
 
-		// Buttons
-		JButton E = new JButton("E");
-		JButton CE = new JButton("CE");
-		JButton C = new JButton("C");
-		JButton clear = new JButton("<");
-		JButton seven = new JButton("7");
-		JButton eig = new JButton("8");
-		JButton nin = new JButton("9");
-		JButton divide = new JButton("/");
-		JButton four = new JButton("4");
-		JButton five = new JButton("5");
-		JButton six = new JButton("6");
-		JButton rest = new JButton("-");
-		JButton one = new JButton("1");
-		JButton two = new JButton("2");
-		JButton tree = new JButton("3");
-		JButton sum = new JButton("+");
-		JButton dat = new JButton(".");
-		JButton zero = new JButton("0");
-		JButton ss = new JButton(",");
-		JButton equal = new JButton("=");
+        // Buttons
+        JButton E = new JButton("E");
+        JButton CE = new JButton("CE");
+        JButton C = new JButton("C");
+        JButton clear = new JButton("*");
+        JButton seven = new JButton("7");
+        JButton eig = new JButton("8");
+        JButton nin = new JButton("9");
+        JButton divide = new JButton("/");
+        JButton four = new JButton("4");
+        JButton five = new JButton("5");
+        JButton six = new JButton("6");
+        JButton rest = new JButton("-");
+        JButton one = new JButton("1");
+        JButton two = new JButton("2");
+        JButton tree = new JButton("3");
+        JButton sum = new JButton("+");
+        JButton dat = new JButton(".");
+        JButton zero = new JButton("0");
+        JButton ss = new JButton("");
+        ss.setEnabled(false);
+        JButton equal = new JButton("=");
 
-		E.addActionListener(this);
-		CE.addActionListener(this);
-		C.addActionListener(this);
-		clear.addActionListener(this);
-		seven.addActionListener(this);
-		eig.addActionListener(this);
-		nin.addActionListener(this);
-		divide.addActionListener(this);
-		four.addActionListener(this);
-		five.addActionListener(this);
-		six.addActionListener(this);
-		rest.addActionListener(this);
-		one.addActionListener(this);
-		two.addActionListener(this);
-		tree.addActionListener(this);
-		sum.addActionListener(this);
-		dat.addActionListener(this);
-		zero.addActionListener(this);
-		ss.addActionListener(this);
-		equal.addActionListener(this);
+        E.addActionListener(this);
+        CE.addActionListener(this);
+        C.addActionListener(this);
+        clear.addActionListener(this);
+        seven.addActionListener(this);
+        eig.addActionListener(this);
+        nin.addActionListener(this);
+        divide.addActionListener(this);
+        four.addActionListener(this);
+        five.addActionListener(this);
+        six.addActionListener(this);
+        rest.addActionListener(this);
+        one.addActionListener(this);
+        two.addActionListener(this);
+        tree.addActionListener(this);
+        sum.addActionListener(this);
+        dat.addActionListener(this);
+        zero.addActionListener(this);
+        ss.addActionListener(this);
+        equal.addActionListener(this);
 
-		// Panel superior (Texto)
-		JPanel Psuperior = new JPanel();
-		Psuperior.setBackground(Color.CIAN);
-		// this.getContentPane().add(Psuperior);// Añadimos el panel al JFrame
+        // Panel superior (Texto)
+        JPanel Psuperior = new JPanel();
+        Psuperior.setBackground(Color.WHITE);
+        resulta = new JLabel("0");
+        Psuperior.add(resulta);
+        // this.getContentPane().add(Psuperior);// Añadimos el panel al JFrame
 
-		// Panel Inferior (Botones)
-		JPanel Inferior = new JPanel();
-		Inferior.setLayout(new GridLayout(5, 4));
-		Inferior.setBackground(Color.DARK_GRAY);
-		// this.getContentPane().add(Inferior);// Añadimos el panel al JFrame
+        // Panel Inferior (Botones)
+        JPanel Inferior = new JPanel();
+        Inferior.setLayout(new GridLayout(5, 4));
+        Inferior.setBackground(Color.DARK_GRAY);
+        // this.getContentPane().add(Inferior);// Añadimos el panel al JFrame
 
-		Inferior.add(E);
-		Inferior.add(CE);
-		Inferior.add(C);
-		Inferior.add(clear);
-		Inferior.add(seven);
-		Inferior.add(eig);
-		Inferior.add(nin);
-		Inferior.add(divide);
-		Inferior.add(four);
-		Inferior.add(five);
-		Inferior.add(six);
-		Inferior.add(rest);
-		Inferior.add(one);
-		Inferior.add(two);
-		Inferior.add(tree);
-		Inferior.add(sum);
-		Inferior.add(dat);
-		Inferior.add(zero);
-		Inferior.add(ss);
-		Inferior.add(equal);
+        Inferior.add(E);
+        Inferior.add(CE);
+        Inferior.add(C);
+        Inferior.add(clear);
+        Inferior.add(seven);
+        Inferior.add(eig);
+        Inferior.add(nin);
+        Inferior.add(divide);
+        Inferior.add(four);
+        Inferior.add(five);
+        Inferior.add(six);
+        Inferior.add(rest);
+        Inferior.add(one);
+        Inferior.add(two);
+        Inferior.add(tree);
+        Inferior.add(sum);
+        Inferior.add(dat);
+        Inferior.add(zero);
+        Inferior.add(ss);
+        Inferior.add(equal);
 
-		setContentPane(contenedor);
-		contenedor.add(Psuperior);
-		contenedor.add(Inferior);
+        setContentPane(contenedor);
+        contenedor.add(Psuperior);
+        contenedor.add(Inferior);
+    }
 
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
 
-	int[] temp = new int[5];
+        if (isNumeric(command) || command.equals("*") || command.equals("/") || command.equals("-") || command.equals("+")) {
+            if (resulta.getText().length() == 1 && resulta.getText().equals("0")) {
+                resulta.setText(command);
+            } else {
+                resulta.setText(resulta.getText() + command);
+            }
+        } else {
+            switch (e.getActionCommand()) {
+                case "E":
+                    if (resulta.getText().length() > 1) {
+                        StringBuilder sb = new StringBuilder(resulta.getText());
+                        sb.deleteCharAt(resulta.getText().length() - 1);
+                        resulta.setText(sb.toString());
+                    } else if (resulta.getText().length() == 1) {
+                        resulta.setText("0");
+                    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		switch (e.getActionCommand()) {
-			case "E":
-				System.out.println(e.getActionCommand());
-				break;
-			case "CE":
-				System.out.println(e.getActionCommand());
-				break;
-			case "C":
-				System.out.println(e.getActionCommand());
-				break;
-			case "<":
-				System.out.println(e.getActionCommand());
-				break;
-			case "clear":
-				System.out.println(e.getActionCommand());
-				break;
-			case "/":
-				System.out.println(e.getActionCommand());
-				break;
-			case "-":
-				System.out.println(e.getActionCommand());
-				break;
-			case "+":
-				System.out.println(e.getActionCommand());
-				int res = 0;
+                    break;
+                case "CE":
+                case "C":
+                    resulta.setText("0");
+                    break;
+                case "=":
+                    ScriptEngineManager manager = new ScriptEngineManager();
+                    ScriptEngine se = manager.getEngineByName("JavaScript");
 
-				for (int j = 0; j < temp.length; j++) {
-					if (temp.length != 0) {
-						for (int i = 0; i < temp.length; i++) {
-							res += temp[i];
-						}
-					} else {
-						System.out.println("No puedo sumar, me falta un numero");
-					}
-				}
+                    try {
+                        Object result = se.eval(resulta.getText());
+                        resulta.setText(result.toString());
+                    } catch (ScriptException error) {
+                        error.printStackTrace();
+                        resulta.setText("ERROR...");
+                    }
+                    break;
+            }
+        }
+    }
 
-				System.out.println(res);
-				break;
-			case "=":
-				System.out.println(e.getActionCommand());
-				break;
-			case "1":
-				System.out.println(e.getActionCommand());
-				temp[0] = 1;
-				for (int i = 0; i < temp.length; i++) {
-					System.out.println("Arreglo tiene [" + temp[i] + "]");
-				}
-				break;
-			case "2":
-				System.out.println(e.getActionCommand());
-				temp[1] = 2;
-
-				for (int i = 0; i < temp.length; i++) {
-					System.out.println("Arreglo tiene [" + temp[i] + "]");
-				}
-
-				break;
-		}
-
-	}
+    private static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
 
 }
 
