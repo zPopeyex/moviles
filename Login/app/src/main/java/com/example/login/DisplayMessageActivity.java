863d097864b1f7c520a4ff848804666b6f08edf1
zPopeyex <stven-457@hotmail.com>
2022-09-07 18:13:00 -0500

    Add next activity in android studio

---
+++ Login/app/src/main/java/com/example/login/DisplayMessageActivity.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        
+    }
+}
