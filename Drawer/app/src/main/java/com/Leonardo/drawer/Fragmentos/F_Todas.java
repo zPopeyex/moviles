74a77f3c0fbe9519e36d01142b586c3ff62e513d
zPopeyex <stven-457@hotmail.com>
2022-10-24 05:27:50 -0500

    Implement Drawer, icons and fragments

---
+++ Drawer/app/src/main/java/com/Leonardo/drawer/Fragmentos/F_Todas.java
@@ -0,0 +1,27 @@
+package com.Leonardo.drawer.Fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.Leonardo.drawer.R;
+
+
+public class F_Todas extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_f__todas, container, false);
+
+
+        return view;
+    }
+}
