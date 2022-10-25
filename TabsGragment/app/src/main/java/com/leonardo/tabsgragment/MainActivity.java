60ef8b0b499c3dac92d516d8acb45b6ac8dd8abb
zPopeyex <stven-457@hotmail.com>
2022-10-25 16:48:55 -0500

    Create tab whit map initial

--- TabsGragment/app/src/main/java/com/leonardo/tabsgragment/MainActivity.java
+++ TabsGragment/app/src/main/java/com/leonardo/tabsgragment/MainActivity.java
@@ -2,21 +2,22 @@ package com.leonardo.tabsgragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
-
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
 
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     ViewPagerAdapter vAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         tabLayout = findViewById(R.id.tabLayout);
         viewPager2 = findViewById(R.id.viewpager);
